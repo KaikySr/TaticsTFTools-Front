1f63cac4b2969137cf88f173f97a673eb6fbbab2
KaikySr <kaikysr2004@gmail.com>
2023-11-16 15:01:44 -0300

    imagens de fundo

--- .bat
+++ .bat
@@ -1,1 +1,6 @@
-npm install --global yarn && yarn install && npx expo start
+npm install --global yarn && 
+yarn add expo && 
+npx expo install react-native-web@~0.18.10 react-dom@18.2.0 @expo/webpack-config@^18.0.1 && 
+npm install @react-navigation/native && 
+npm install @react-navigation/stack && 
+npx expo start
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,29 +1,50 @@
-import { StyleSheet, View} from 'react-native';
+import { StyleSheet, View, Image, Text, ImageBackground} from 'react-native';
+const Logo = require('../../../assets/logo.png')
+const imageBack = require('../../../assets/backImageLogin.png')
 
 export default function Login() {
     return (
-      <View style={styles.backgroundImage}>
+      <ImageBackground style={styles.backgroundImage} source={imageBack} resizeMode="cover">
         <View style={styles.logo}>
-
+          <Image source={Logo} style={styles.img} /> 
         </View>
         <View style={styles.inputs}>
-
+          <Text>oi</Text>
         </View>
         <View style={styles.start}>
-
+          <Text>teste</Text>
         </View>
         <View style={styles.create}>
 
         </View>
-      </View>
+      </ImageBackground>
     );
   }
 
   const styles = StyleSheet.create({
     backgroundImage: {
       flex: 1,
-      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'top',
+    },
+    logo: {
+      display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
+      width: 400,
+      height: 231
+    }, 
+    img: {
+      width: 306,
+      height: 231
     },
+    inputs: {
+     
+    }, 
+    start: {
+      
+    }, 
+    create: {
+    
+    }, 
   });
