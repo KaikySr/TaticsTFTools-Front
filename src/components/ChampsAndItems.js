b0e3509a36f7d98596c0047185a1a0f17b0d271c
KaikySr <kaikysr2004@gmail.com>
2023-11-22 17:07:42 -0300

    modal of a champs screen check

--- src/components/ChampsAndItems.js
+++ src/components/ChampsAndItems.js
@@ -1,49 +1,78 @@
-import { useState } from 'react';
-import { StyleSheet, View, TouchableOpacity, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TouchableOpacity, Text, ScrollView } from 'react-native';
 import Modal from 'react-native-modal';
-// import Items from './items';
+
 
 export default function ChampsAndItems() {
 
   const [isChampsActive, setIsChampsActive] = useState(false)
   const [isItemsActive, setIsItemsActive] = useState(false)
 
-  const [modalVisible, setModalVisible] = useState(false);
+  const [modalChampsVisible, setmodalChampsVisible] = useState(false);
+  const [modalItemsVisible, setmodalItemsVisible] = useState(false);
 
   const handleChampClick = () => {
     setIsChampsActive(true)
     setIsItemsActive(false)
-    setModalVisible(false)
+    setmodalItemsVisible(false)
+    setmodalChampsVisible(true)
   }
 
   const handleItemClick = () => {
     setIsChampsActive(false)
     setIsItemsActive(true)
-    setModalVisible(true)
+    setmodalItemsVisible(true)
+    setmodalChampsVisible(false)
   }
 
   return (
     <View style={styles.browser}>
       <View style={styles.champs}>
-        <TouchableOpacity onPress={handleChampClick} style={isChampsActive ? styles.buttonChampsClicked : styles.buttonChamps}>C<br />H<br />A<br />M<br />P<br />S</TouchableOpacity>
+        <TouchableOpacity onPress={handleChampClick} style={isChampsActive ? styles.buttonChampsClicked : styles.buttonChamps}><Text style={styles.textBrowser}>C<br />H<br />A<br />M<br />P<br/>S</Text></TouchableOpacity>
       </View>
       <View style={styles.items}>
-        <TouchableOpacity onPress={handleItemClick} style={isItemsActive ? styles.buttonItemsClicked : styles.buttonItems}>I<br />T<br />E<br />M<br />S</TouchableOpacity>
+        <TouchableOpacity onPress={handleItemClick} style={isItemsActive ? styles.buttonItemsClicked : styles.buttonItems}><Text style={styles.textBrowser}>I<br />T<br />E<br />M<br/>S</Text></TouchableOpacity>
       </View>
 
       <Modal
-        isVisible={modalVisible}
+        isVisible={modalChampsVisible}
         animationIn={'slideInLeft'}
         animationOut={'slideOutLeft'}
         backdropOpacity={0}
         style={{ marginLeft: '7vw' }}
-        shouldFocusAfterRender={false}
-      >
+        shouldFocusAfterRender={false}>
         <View style={styles.modalView}>
-          <View style={styles.itemsModal}>hehe</View>
-          <View style={styles.button}><TouchableOpacity style={styles.close} onPress={() => setModalVisible(!modalVisible)}>X</TouchableOpacity></View>
-          
+          <ScrollView horizontal style={styles.champsModal}> 
+            <View style={styles.raridadeComum}>
+              
+            </View>
+            <View style={styles.raridadeRara}>
+              
+            </View>
+            <View style={styles.raridadeMuitoRara}>
+              
+            </View>
+            <View style={styles.raridadeEpica}>
+              
+            </View>
+            <View style={styles.raridadeLendaria}>
+             
+            </View>
+          </ScrollView>
+          <View style={styles.button}><TouchableOpacity style={styles.close} onPress={() => setmodalChampsVisible(!modalChampsVisible)}><Text>X</Text></TouchableOpacity></View>
+        </View>
+      </Modal>
 
+      <Modal
+        isVisible={modalItemsVisible}
+        animationIn={'slideInLeft'}
+        animationOut={'slideOutLeft'}
+        backdropOpacity={0}
+        style={{ marginLeft: '7vw' }}
+        shouldFocusAfterRender={false}>
+        <View style={styles.modalView}>
+          <View style={styles.itemsModal}></View>
+          <View style={styles.button}><TouchableOpacity style={styles.close} onPress={() => setmodalItemsVisible(!modalItemsVisible)}><Text>X</Text></TouchableOpacity></View>
         </View>
       </Modal>
     </View>
@@ -62,6 +91,14 @@ const styles = StyleSheet.create({
     width: "6vw",
     height: "50vh",
   },
+  textBrowser: {
+    color: "white",
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    fontSize: 20
+  },
   buttonChamps: {
     width: "100%",
     height: "100%",
@@ -71,7 +108,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     textAlign: 'center',
-    fontSize: 25
+    fontSize: 20
   },
   buttonChampsClicked: {
     width: "100%",
@@ -82,7 +119,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     textAlign: 'center',
-    fontSize: 25,
+    fontSize: 20,
     borderWidth: 5,
     borderColor: "white"
   },
@@ -95,7 +132,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     textAlign: 'center',
-    fontSize: 25
+    fontSize: 20
   },
   buttonItemsClicked: {
     width: "100",
@@ -106,11 +143,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     textAlign: 'center',
-    fontSize: 25,
+    fontSize: 20,
     borderWidth: 5,
     borderColor: "white"
   },
-
   modalView: {
     width: '80vw',
     height: '100vh',
@@ -121,10 +157,16 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
   },
-  itemsModal: {
+  champsModal: {
     width: '95%',
     height: '95%',
-    padding: 5,
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  itemsModal: {
+    backgroundColor: 'white',
+    width: '95%',
+    height: '95%',
   },
   button: {
     width: '5%',
@@ -143,5 +185,35 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: '50%'
-  }
+  },
+  raridadeComum: {
+    width: '38vw',
+    height: '100%',
+    backgroundColor: '#BCBCBC',
+    borderRadius: 15
+  },
+  raridadeRara: {
+    width: '38vw',
+    height: '100%',
+    backgroundColor: '#77DD77',
+    borderRadius: 15
+  },
+  raridadeMuitoRara: {
+    width: '38vw',
+    height: '100%',
+    backgroundColor: '#84B6F4',
+    borderRadius: 15
+  },
+  raridadeEpica: {
+    width: '38vw',
+    height: '100%',
+    backgroundColor: '#711BFC',
+    borderRadius: 15
+  },
+  raridadeLendaria: {
+    width: '38vw',
+    height: '100%',
+    backgroundColor: '#EFB810',
+    borderRadius: 15
+},
 });
